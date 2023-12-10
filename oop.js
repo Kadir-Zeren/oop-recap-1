ee7d21f91e1e44a47d959b8be23a66106dac451d
kadir-zeren <k4dzeren@gmail.com>
2023-12-10 14:31:27 +0300

    v3

--- oop.js
+++ oop.js
@@ -10,6 +10,34 @@ const exampleObject = {
 // console.log(exampleObject.propertyName);
 // console.log(exampleObject.methodName());
 
+// const Car = {
+//   brand: "Ford",
+//   model: "Mustang",
+//   year: 1967,
+//   isAutoGear: true,
+//   colors: ["white", "red"],
+//   details: {
+//     color: "red",
+//     color2: "white",
+//     engineSize: 4900,
+//   },
+//   startEngine: function () {
+//     return "Motor çalıştı";
+//   },
+// };
+
+// console.log(Car.brand);
+// console.log(Car.colors);
+// console.log(Car.colors[0]);
+// console.log(Car.details);
+// console.log(Car.details.engineSize);
+// console.log(Car.startEngine());
+
+// console.log(Car["brand"]);
+// console.log(Car.details["engineSize"]);
+// console.log(Car["details"]["engineSize"]);
+// console.log(Car["startEngine"]());
+
 const Car = {
   brand: "Ford",
   model: "Mustang",
@@ -24,16 +52,15 @@ const Car = {
   startEngine: function () {
     return "Motor çalıştı";
   },
+  getDetails: function () {
+    // return this;
+    // return this.brand + " " + this.model + " " + this.year;
+    return this.startEngine();
+  },
+  arrowFunc: () => {
+    return this;
+  },
 };
 
-// console.log(Car.brand);
-// console.log(Car.colors);
-// console.log(Car.colors[0]);
-// console.log(Car.details);
-// console.log(Car.details.engineSize);
-// console.log(Car.startEngine());
-
-console.log(Car["brand"]);
-console.log(Car.details["engineSize"]);
-console.log(Car["details"]["engineSize"]);
-console.log(Car["startEngine"]());
+// console.log(Car.getDetails());
+console.log(Car.arrowFunc());
