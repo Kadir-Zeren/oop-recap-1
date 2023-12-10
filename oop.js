bb9632151b9933213709e787a14cbea4b9f6f521
kadir-zeren <k4dzeren@gmail.com>
2023-12-10 15:30:16 +0300

    v8

--- oop.js
+++ oop.js
@@ -78,48 +78,74 @@ const exampleObject = {
 // const newArr = [...testArray, "new-value", "new-value2"];
 // console.log(newArr);
 
-const Car = {
-  brand: "Ford",
-  model: "Mustang",
-  year: 1967,
-  isAutoGear: true,
-  colors: ["white", "red"],
-  details: {
-    color: "red",
-    color2: "white",
-    engineSize: 4900,
-  },
-  startEngine: function () {
+// const Car = {
+//   brand: "Ford",
+//   model: "Mustang",
+//   year: 1967,
+//   isAutoGear: true,
+//   colors: ["white", "red"],
+//   details: {
+//     color: "red",
+//     color2: "white",
+//     engineSize: 4900,
+//   },
+//   startEngine: function () {
+//     return "Motor çalıştı";
+//   },
+// };
+
+// const { year: modelYear, model: newName, brand, ...otherItems } = Car;
+
+// console.log(modelYear, newName, brand);
+// // console.log(otherItems);
+
+// console.log(Car);
+
+// const newObj = {
+//   ...Car.colors,
+//   newKey: "new-value",
+// };
+
+// console.log(newObj);
+
+// const json = JSON.stringify(Car);
+// console.log(typeof json, json);
+
+// const newObj2 = JSON.parse(json);
+// console.log(typeof newObj2, newObj2);
+
+// const arr = Object.entries(Car);
+
+// console.log(arr);
+
+// const arr2 = Object.values(Car);
+// console.log(arr2);
+
+// const arr3 = Object.keys(Car);
+// console.log(arr3);
+
+const PascalCaseNmaed = function () {
+  this.property = "value";
+};
+
+const CarConstructor = function (brand, model, year = 2011) {
+  this.brand = brand;
+  this.model = model;
+  this.year = year;
+  this.isRunning = false;
+  this.startEngine = function () {
+    this.isRunning = true;
     return "Motor çalıştı";
-  },
+  };
 };
 
-const { year: modelYear, model: newName, brand, ...otherItems } = Car;
+const newCar = new CarConstructor("ford", "mustang", 1967);
 
-console.log(modelYear, newName, brand);
-// console.log(otherItems);
+console.log(newCar);
 
-console.log(Car);
+const newCar2 = new CarConstructor("toyota", "corolla");
+console.log(newCar2);
 
-const newObj = {
-  ...Car.colors,
-  newKey: "new-value",
-};
-
-console.log(newObj);
-
-const json = JSON.stringify(Car);
-console.log(typeof json, json);
-
-const newObj2 = JSON.parse(json);
-console.log(typeof newObj2, newObj2);
-
-const arr = Object.entries(Car);
-
-console.log(arr);
-
-const arr2 = Object.values(Car);
-console.log(arr2);
-
-const arr3 = Object.keys(Car);
-console.log(arr3);
+console.log(newCar2.isRunning);
+console.log(newCar2.startEngine());
+console.log(newCar2.isRunning);
