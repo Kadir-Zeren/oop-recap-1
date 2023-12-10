c7aa7111cd2a1d78ad6a3c98ffd4c5f3e5aca312
kadir-zeren <k4dzeren@gmail.com>
2023-12-10 14:36:13 +0300

    v4

--- oop.js
+++ oop.js
@@ -38,29 +38,39 @@ const exampleObject = {
 // console.log(Car["details"]["engineSize"]);
 // console.log(Car["startEngine"]());
 
-const Car = {
-  brand: "Ford",
-  model: "Mustang",
-  year: 1967,
-  isAutoGear: true,
-  colors: ["white", "red"],
-  details: {
-    color: "red",
-    color2: "white",
-    engineSize: 4900,
-  },
-  startEngine: function () {
-    return "Motor çalıştı";
-  },
-  getDetails: function () {
-    // return this;
-    // return this.brand + " " + this.model + " " + this.year;
-    return this.startEngine();
-  },
-  arrowFunc: () => {
-    return this;
-  },
-};
+// const Car = {
+//   brand: "Ford",
+//   model: "Mustang",
+//   year: 1967,
+//   isAutoGear: true,
+//   colors: ["white", "red"],
+//   details: {
+//     color: "red",
+//     color2: "white",
+//     engineSize: 4900,
+//   },
+//   startEngine: function () {
+//     return "Motor çalıştı";
+//   },
+//   getDetails: function () {
+//     // return this;
+//     // return this.brand + " " + this.model + " " + this.year;
+//     return this.startEngine();
+//   },
+//   arrowFunc: () => {
+//     return this;
+//   },
+// };
 
-// console.log(Car.getDetails());
-console.log(Car.arrowFunc());
+// // console.log(Car.getDetails());
+// console.log(Car.arrowFunc());
+
+const testArray = ["value0", "value1", "value2", "value3"];
+
+// const var0 = testArray[0];
+
+// const [firstItem, secondItem] = testArray;
+// console.log(firstItem, secondItem);
+
+const [first, second, third, ...others] = testArray;
+console.log(first, second, third, others);
